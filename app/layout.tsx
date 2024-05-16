7ab5fe2a5c9982db36f4ac4ec98e398a12c17777
attidack <attidack@gmail.com>
2024-05-16 17:44:42 -0600

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ export const metadata: Metadata = {
     template: '%s | Acme Dashboard',
     default: 'Acme Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  description: 'Next.js Dashboard built with App Router.',
 };
 
 export default function RootLayout({
